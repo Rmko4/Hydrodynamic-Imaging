2d92855e632dfe8b04cc3a8cc26d2281fd588d73
Remco <remco-leijenaar@live.com>
2021-04-14 22:46:31 +0200

    Cubic scaling

--- src/main.py
+++ src/main.py
@@ -23,9 +23,35 @@ def main():
     samples_u, samples_y = pfenv.sample_sensor_data()
     # sampling.plot(samples_y, "m")
 
+    def _cube_root(x):
+        sign = tf.sign(x)
+        x = sign * tf.pow(abs(x), 1./3.)
+        return x
+
+    def _normalize(x, scale):
+        c = x.numpy()
+        x = _cube_root(x)
+        a = x.numpy()
+        output = scale * x
+        b = output.numpy()
+        return output
+
+    def call(inputs):
+        u_x, u_y = tf.split(inputs, 2, axis=1)
+        scale = np.math.pow(pfenv.y_offset, 3) / (pfenv.W * np.math.pow(pfenv.a, 3))
+
+        u_x = _normalize(u_x, 2*scale)
+        u_y = _normalize(u_y, scale)
+
+        outputs = tf.concat([u_x, u_y], 1)
+        return outputs
+
+    print(samples_u[0])
+    print(call(tf.convert_to_tensor(samples_u))[0])
+
     mlp = MLP(pfenv)
     mlp.compile()
-    mlp.fit(samples_u, samples_y, batch_size=500, validation_split=0.2, epochs=100)
+    mlp.fit(samples_u, samples_y, batch_size=32, validation_split=0.2, epochs=100000)
     a = np.array([samples_u[0]])
     # print(mlp.predict(a))
     # print(samples_y[0])
--- src/mlp.py
+++ src/mlp.py
@@ -10,13 +10,43 @@ class RescaleProfile(keras.layers.Layer):
         super(RescaleProfile, self).__init__(**kwargs)
 
     def _rescale(self, x):
-        return x/tf.reduce_max(tf.abs(x))
+        abs_max = tf.reduce_max(tf.abs(x), axis=1)
+        output = x/tf.reshape(abs_max, (-1,1))
+        return output
 
     def call(self, inputs):
-        u_x, u_y = tf.split(inputs, 2)
+        u_x, u_y = tf.split(inputs, 2, axis=1)
         u_x = self._rescale(u_x)
         u_y = self._rescale(u_y)
-        outputs = tf.concat([u_x, u_y], 0)
+        outputs = tf.concat([u_x, u_y], 1)
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+
+class CubicRootNormalize(keras.layers.Layer):
+    def __init__(self, pfenv : PotentialFlowEnv, **kwargs):
+        super(CubicRootNormalize, self).__init__(**kwargs)
+        self.pfenv = pfenv
+
+    def _cube_root(self, x):
+        sign = tf.sign(x)
+        x = sign * tf.pow(abs(x), 1./3.)
+        return x
+
+    def _normalize(self, x, scale):
+        x = self._cube_root(x)
+        output = scale * x
+        return output
+
+    def call(self, inputs):
+        u_x, u_y = tf.split(inputs, 2, axis=1)
+        scale = np.math.pow(self.pfenv.y_offset, 3) / (self.pfenv.W * np.math.pow(self.pfenv.a, 3))
+
+        u_x = self._normalize(u_x, 0.5*scale)
+        u_y = self._normalize(u_y, scale)
+
+        outputs = tf.concat([u_x, u_y], 1)
         return outputs
 
     def compute_output_shape(self, input_shape):
@@ -31,14 +61,15 @@ class MLP(keras.Sequential):
         input_shape = (2*s_bar.size,)
         self.add(layers.InputLayer(input_shape=input_shape))
         self.add(RescaleProfile())
+        # self.add(CubicRootNormalize(pfenv))
         # Make layer that scales the u_x_bar and u_y_bar separately on perhaps current max* or overal max.
 
-        # scale = np.math.pow(pfenv.y_offset, 3) / (pfenv.W * np.math.pow(pfenv.a, 3))
         # self.add(layers.experimental.preprocessing.Rescaling(scale=scale, input_shape=input_shape))
         self.add(layers.Dense(100, activation="sigmoid"))
         self.add(layers.Dense(pfenv.Y_BAR_SIZE))
         # Either transform and wrap phi. Or use special loss function.
         self.summary()
+
 
     def compile(self):
         super(MLP, self).compile(
@@ -46,7 +77,8 @@ class MLP(keras.Sequential):
             # Loss function to minimize
             loss=keras.losses.MeanAbsoluteError(),
             # List of metrics to monitor
-            metrics=[keras.metrics.MeanAbsoluteError()]
+            metrics=[keras.metrics.MeanAbsoluteError()],
+            # run_eagerly=True
         )
 
 
