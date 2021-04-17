0c39ba93f15b41a2bdeb6ef603cccfacbc86e5c6
Remco <remco-leijenaar@live.com>
2021-04-17 17:00:44 +0200

    Added normalized MSE using cos and sin of phi angle. Added default training step by copying the default training behaviour to an overloaded train_step().

--- src/mlp.py
+++ src/mlp.py
@@ -54,7 +54,6 @@ class CubicRootNormalize(keras.layers.Layer):
     def compute_output_shape(self, input_shape):
         return input_shape
 
-
 def MED_p(y_true, y_pred):
     L2_norm = tf.sqrt(tf.reduce_sum(tf.square(y_true[:, 0:2] - y_pred[:, 0:2]), axis=-1))
     return tf.reduce_mean(L2_norm)
@@ -62,18 +61,20 @@ def MED_p(y_true, y_pred):
 def MDE_phi(y_true, y_pred):
     phi_e = y_true[:, 2] - y_pred[:, 2]
     abs_atan2 = tf.abs(tf.atan2(tf.sin(phi_e), tf.cos(phi_e)))
-    return tf.reduce_mean(abs_atan2)
+    return 2 * tf.reduce_mean(abs_atan2)
 
 def MSE(y_true, y_pred):
     return tf.reduce_mean(tf.square(y_true - y_pred))
 
 
 class MLP(keras.Sequential):
+
     def __init__(self, pfenv: PotentialFlowEnv):
         super(MLP, self).__init__()
         self.pfenv = pfenv
         s_bar = pfenv.getSensor().s_bar
         input_shape = (2*s_bar.size,)
+        self.D_2SQ = 1/self.pfenv.dimensions[0]
         self.add(layers.InputLayer(input_shape=input_shape))
         self.add(RescaleProfile())
         # self.add(CubicRootNormalize(pfenv))
@@ -89,26 +90,56 @@ class MLP(keras.Sequential):
         super(MLP, self).compile(
             optimizer=keras.optimizers.Adam(),  # Optimizer
             # Loss function to minimize
-            loss=keras.losses.MeanSquaredError(),
+            loss=[self._MSE_normalized, keras.losses.MeanSquaredError()],
             # List of metrics to monitor
-            metrics=[keras.losses.MSE, MSE, MED_p, MDE_phi],
+            metrics=[MED_p, MDE_phi],
         )
 
-    def _normalize_y(y_bar):
-        return tf.constant([y_bar[:, 0], y_bar[:, 1], tf.cos(y_bar[:, 2]), tf.sin(y_bar[:, 2])])
+    def train_step(self, data):
+        x, y = data
 
-    def _MSE_normalize():
-        return
+        with tf.GradientTape() as tape:
+            y_pred = self(x, training=True)  # Forward pass
+            # Compute the loss value
+            # (the loss function is configured in `compile()`)
+            loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+
+        # Compute gradients
+        trainable_vars = self.trainable_variables
+        gradients = tape.gradient(loss, trainable_vars)
+        # Update weights
+        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+        # Update metrics (includes the metric that tracks the loss)
+        self.compiled_metrics.update_state(y, y_pred)
+        # Return a dict mapping metric names to current value
+        return {m.name: m.result() for m in self.metrics}
+
+
+    def _normalize_y(self, y_bar):
+        return  tf.constant([y_bar[:, 0], y_bar[:, 1], tf.cos(y_bar[:, 2]), tf.sin(y_bar[:, 2])])
+
+    def _MSE_normalized(self, y_true, y_pred):
+        # Reduce sum is faster
+        MSE_p = self.D_2SQ * tf.reduce_mean(tf.square(y_true[:, 0:2] - y_pred[:, 0:2]))
+        MSE_phi = tf.reduce_mean(tf.square(tf.cos(y_true[:, 2]) - tf.cos(y_pred[:, 2])))
+        MSE_phi += tf.reduce_mean(tf.square(tf.sin(y_true[:, 2]) - tf.sin(y_pred[:, 2])))
+        MSE_out = 0.5 * (MSE_p + 0.5 * MSE_phi)
+        return MSE_out
 
 
 def main():
-    # m = MLP(PotentialFlowEnv(sensor=SensorArray(8)))
-    y_true = tf.constant([[1., 1., 0.], [2., 3., 3.]])
-    y_pred = tf.constant([[2., 3., 6], [2., 3., 3.]])
-    # print(rMSE_p(y_true, y_pred))
-    # print(MSE_p(y_true, y_pred))
-    print(MED_p(y_true, y_pred))
+    m = MLP(PotentialFlowEnv(sensor=SensorArray(8)))
 
+    y_true = tf.constant([[1., 1., 3], [2., 3., 3.]])
+    y_pred = tf.constant([[2., 3., 0.], [2., 3., 3.]])
+
+
+    print(MED_p(y_true, y_pred))
+    print(MDE_phi(y_true, y_pred))
+    print(MDE_phi(y_true, y_pred))
+
+    print(MSE(y_true, y_pred))
+    print(m._MSE_normalize(y_true, y_pred))
 
 if __name__ == "__main__":
     main()
