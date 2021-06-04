7ec139dedf90599e502400cf4ce57a247c447003
Remco <remco-leijenaar@live.com>
2021-06-04 18:18:51 +0200

    dipole field denser field

--- src/dipole_field.py
+++ src/dipole_field.py
@@ -3,19 +3,19 @@ import matplotlib.pyplot as plt
 
 
 def plot_dipole_field():
-    slc = slice(-1, 1, 100j)
+    slc = slice(-.5, .5, 100j)
     Y, X = np.mgrid[slc, slc]
 
-    r = np.stack((Y, X), axis=-1)
+    r = np.stack((X, Y), axis=-1)
     r_norm = np.expand_dims(np.linalg.norm(r, axis=-1), axis=-1)
 
     a = 1
-    w = np.array([0, 1])
+    w = np.array([1, 0])
 
     w_dot_r = np.expand_dims(np.tensordot(w, r, axes=(0, 2)), axis=-1)
     v = a**3 / (2 * r_norm**3) * (-w + 3 * r * w_dot_r / (r_norm**2))
-    V = v[:, :, 0]
-    U = v[:, :, 1]
+    U = v[:, :, 0]
+    V = v[:, :, 1]
 
     speed = np.sqrt(U**2 + V**2)
     lw = 5*np.log(speed) / np.log(speed.max())
@@ -23,10 +23,23 @@ def plot_dipole_field():
     # lines = np.linspace(0., 1., 20)
     # seed_points = np.array([lines, lines])
     # start_points=seed_points.T
-    plt.streamplot(X, Y, U, V, density=1, color="grey",
-                   linewidth=lw)
+    plt.streamplot(X, Y, U, V, density=1.8, color='grey',
+                   linewidth=lw, arrowsize=.7, zorder=-1)
+    plt.scatter([0], [0], color='black', s=100., zorder=1)
+    plt.scatter([0], [0], color='grey', s=60., zorder=2)
+    plt.quiver(0, 0, w[0], w[1], zorder=1, scale=12)
+
     ax = plt.gca()
     ax.set_aspect("equal")
+
+    plt.tick_params(
+        axis='both',
+        which='both',
+        bottom=False,
+        left=False,
+        labelbottom=False,
+        labelleft=False)
+
     plt.show()
     pass
 
